c5f7926c6b1b594d6e38cb4e61d6c2abb38209bd
Mah97 <mah97.2022@gmail.com>
2023-02-22 18:50:01 -0500

    express-ejs-layouts configured

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
+const expressLayouts = require('express-ejs-layouts')
 const app = express ();
 const db = mongoose.connection;
 require('dotenv').config()
@@ -44,14 +45,24 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
+// express-ejs-layouts
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts)
 
 //___________________
 // Routes
 //___________________
 //localhost:3000
-app.get('/' , (req, res) => {
-  res.render('index.ejs');
-});
+// app.get('/' , (req, res) => {
+//   res.render('index.ejs');
+// });
+
+const indexRouter = require('./controller/index')
+
+// use routes
+app.use('/', indexRouter)
 
 //___________________
 //Listener
